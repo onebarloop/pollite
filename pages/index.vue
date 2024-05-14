f851f60cde9264bfaf842f7c18edd6a47054e454
onebarloop <alexanderbayer@posteo.de>
2024-05-14 21:15:39 +0200

    add loading

--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,26 @@
 <script setup lang="ts">
 const title: Ref<null | string> = ref(null);
 
-const { data, refresh, pending } = await useFetch('/api/crudtest');
+const loading: Ref<boolean> = ref(false);
+
+const { data, refresh } = await useFetch('/api/crudtest');
 
 const create = async () => {
+  loading.value = true;
   await useFetch('/api/crudtest', {
     method: 'POST',
     body: { data: title.value },
   });
   refresh();
   title.value = null;
+  loading.value = false;
 };
 </script>
 
 <template>
   <h1>Create Poll</h1>
   <input v-model="title" placeholder="Name of the poll" />
-  <button @click="create" :disabled="pending">Create</button>
+  <button @click="create" :disabled="loading">Create</button>
   <ul>
     <li v-for="poll in data?.dbRes">
       {{ poll.title }}
